b7d63360f877d9f9ad0f3f2f06c6b4f949416570
Rehemamarjan <rehemamarjan50@gmail.com>
2018-05-24 16:53:38 +0300

    added functionArray.js

---
+++ functionArray.js
@@ -0,0 +1,14 @@
+function array([student]){
+
+	var student = ["rehema","marjan","ramadhan","juma"]
+
+	return student[i]
+
+
+}
+
+result = array()
+
+console.log(result)
+
+const answer = (["rehema","marjan","ramadhan"]) => {return student[i]};
